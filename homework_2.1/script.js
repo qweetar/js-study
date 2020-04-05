f03adcbb2db83ddd17a536fa170448151e822b29
Sergey Kuzmenok <3886422@gmail.com>
2020-04-05 08:39:12 +0300

    fixing problems

--- homework_2.1/script.js
+++ homework_2.1/script.js
@@ -2,22 +2,25 @@ var myArray = [ 5, 7,
   [ 4, [2], 8, [1,3], 2],
   [ 9, []],
   1, 8
-]
+];
 
-var sum = 0;
 
 function treesum(n) {
-  for (var i = 0; i < n.length; i++) {
-    if (Array.isArray(n[i])) {
-    treesum(n[i]);
-    } else {
-      console.log("i = " + n[i]);
-      sum+= Number(n[i]);
-      console.log("sum = " + sum);
+  var sum = 0;
+  var countArray = n;
+  counter(countArray);
+  function counter(n) {
+    for (var i = 0; i < n.length; i++) {
+      if (Array.isArray(n[i])) {
+        counter(n[i]);
+      } else {
+        console.log("i = " + n[i]);
+        sum+= Number(n[i]);
+        console.log("sum = " + sum);
+      }
     }
   }
+  return sum;
 }
 
-treesum(myArray);
-
-alert(sum);
+alert(treesum(myArray));
--- homework_2.2/script.js
+++ homework_2.2/script.js
@@ -1,17 +1,17 @@
+function vowelCounter() {
 const vowels = ["а", "о", "у", "ю", "е", "ё", "и", "ы", "я", "э", "А", "О", "У", "Ю", "Е", "Ё", "И", "Ы", "Я", "Э"];
 
  var text = prompt("Введите текст на русском языке");
 
  var countVowels = 0;
 
- var chars = text.split("");
-
- for (var i = 0; i < chars.length; i++) {
-   for (var j = 0; j < vowels.length; j++) {
-     if (chars[i] == vowels[j]) {
+ for (var i = 0; i < vowels.length; i++) {
+     if (text.includes(vowels[i])) {
        countVowels++;
-     }
    }
  }
 
  alert("Количество гласных = " + countVowels);
+}
+
+vowelCounter();
