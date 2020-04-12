a5345d4a56c70d9f94564a353f09c74c981cd763
Sergey Kuzmenok <3886422@gmail.com>
2020-04-12 07:37:12 +0300

    additionl verification

--- homework_3.1/script.js
+++ homework_3.1/script.js
@@ -8,12 +8,22 @@ function cutSpace(text) {
   var startChar = 0;
   var endChar = text.length;
 
+  if (text.length == 0) {
+    console.log("Строка пустая");
+    return text;
+  }
+
   for (var i = 0; i < text.length; i++) {
     if (text[i] == " ") {
       startChar = 1 + i;
     } else {
       break;
     }
+  }
+
+  if (startChar == text.length && startChar > 0) {
+    console.log("Строка состоит только из пробелов");
+    return text;
   }
 
   for (var j = text.length - 1; j >= 0; j--) {
@@ -24,8 +34,13 @@ function cutSpace(text) {
     }
   }
 
-  var cleanText = text.slice(startChar, endChar);
+  if (startChar == 0 && endChar == text.length) {
+    console.log("В начале и конце строки нет пробелов");
+    return text;
+  }
 
+  var cleanText = text.slice(startChar, endChar);
+  console.log("В строке вырезаны пробелы");
   return cleanText;
 }
 
