2a33757a83ab8e8fdf7de72195f56d8ef2ee7f95
Sergey Kuzmenok <3886422@gmail.com>
2020-06-30 05:52:41 +0300

    Homework N.60 web test

---
+++ homework_N.60/pushScript.js
@@ -0,0 +1,77 @@
+
+//Script for pushind data to server
+
+var info;
+
+info = form1;
+storeInfo();
+
+info = form2;
+storeInfo();
+
+
+var form1=
+[
+{label:'Название сайта:',kind:'longtext',name:'sitename'},
+{label:'URL сайта:',kind:'longtext',name:'siteurl'},
+{label:'Посетителей в сутки:',kind:'number',name:'visitors'},
+{label:'E-mail для связи:',kind:'shorttext',name:'email'},
+{label:'Рубрика каталога:',kind:'combo',name:'division',
+variants:[{text:'здоровье',value:1},{text:'домашний уют',value:2},{text:'бытовая техника',value:3}]},
+{label:'Размещение:',kind:'radio',name:'payment',
+variants:[{text:'бесплатное',value:1},{text:'платное',value:2},{text:'VIP',value:3}]},
+{label:'Разрешить отзывы:',kind:'check',name:'votes'},
+{label:'Описание сайта:',kind:'memo',name:'description'},
+{label:'Опубликовать:',kind:'submit'},
+];
+var form2=
+[
+{label:'Фамилия:',kind:'longtext',name:'lastname'},
+{label:'Имя:',kind:'longtext',name:'firstname'},
+{label:'Отчество:',kind:'longtext',name:'secondname'},
+{label:'Возраст:',kind:'number',name:'age'},
+{label:'Зарегистрироваться:',kind:'submit'},
+];
+
+var ajaxHandlerScript = "https://fe.it-academy.by/AjaxStringStorage2.php";
+var updatePassword;
+var stringName = "KUZMENOK_DYN_FORM";
+
+function storeInfo() {
+  updatePassword = Math.random();
+  $.ajax({
+    url: ajaxHandlerScript, type: "POST", cache: false, dataType: "json",
+    data: {f: "LOCKGET", n: stringName, p: updatePassword},
+    success: lockGetReady, error: errorHandler
+  })
+}
+
+function lockGetReady(callresult) {
+  if (callresult.error != undefined) {
+    alert(callresult.error);
+  } else {
+    var newInfo = {
+      form1: form1,
+      form2: form2
+
+    }
+
+    $.ajax({
+      url: ajaxHandlerScript, type: "POST", cache: false, dataType: "json",
+      data: {f: "UPDATE", n: stringName, v: JSON.stringify(newInfo), p: updatePassword},
+      success: updateReady, error: errorHandler
+    })
+  }
+}
+
+
+function updateReady(callresult) {
+  if (callresult.error != undefined) {
+    alert(callresult.error);
+  }
+}
+
+
+function errorHandler(jqXHR, statusStr, errorStr) {
+  alert(statusStr + " " + errorStr);
+}
--- homework_N.60/script.js
+++ homework_N.60/script.js
@@ -1,29 +1,18 @@
 
 "use strict";
 
-var info;
-var infoFromServer;
+var form1;
+var form2;
 
-function addFirstArray() {
-  info = form1;
-  storeInfo();
-}
-function addSecondArray() {
-  info = form2;
-  storeInfo();
-}
-
-function renderForm() {
   readInfo();
-  addHtml();
-}
 
+  // console.log(form1);
+  addHtml(form1, "myForm1");
+  addHtml(from2, "myForm2");
 
+function addHtml(form, formId) {
 
-
-function addHtml() {
-
-var htmlArray = infoFromServer;
+var htmlArray = form;
 
         var str='';
         str+="<div>";
@@ -57,62 +46,15 @@ var htmlArray = infoFromServer;
 
         str+="</div>";
 
-        var contElem=document.getElementById('myForm');
+        var contElem=document.getElementById(formId);
         contElem.innerHTML=str;
         console.log(contElem.innerHTML);
 }
 
-var form1=
-[
-{label:'Название сайта:',kind:'longtext',name:'sitename'},
-{label:'URL сайта:',kind:'longtext',name:'siteurl'},
-{label:'Посетителей в сутки:',kind:'number',name:'visitors'},
-{label:'E-mail для связи:',kind:'shorttext',name:'email'},
-{label:'Рубрика каталога:',kind:'combo',name:'division',
-variants:[{text:'здоровье',value:1},{text:'домашний уют',value:2},{text:'бытовая техника',value:3}]},
-{label:'Размещение:',kind:'radio',name:'payment',
-variants:[{text:'бесплатное',value:1},{text:'платное',value:2},{text:'VIP',value:3}]},
-{label:'Разрешить отзывы:',kind:'check',name:'votes'},
-{label:'Описание сайта:',kind:'memo',name:'description'},
-{label:'Опубликовать:',kind:'submit'},
-];
-var form2=
-[
-{label:'Фамилия:',kind:'longtext',name:'lastname'},
-{label:'Имя:',kind:'longtext',name:'firstname'},
-{label:'Отчество:',kind:'longtext',name:'secondname'},
-{label:'Возраст:',kind:'number',name:'age'},
-{label:'Зарегистрироваться:',kind:'submit'},
-];
 
 var ajaxHandlerScript = "https://fe.it-academy.by/AjaxStringStorage2.php";
 var updatePassword;
 var stringName = "KUZMENOK_DYN_FORM";
-
-function storeInfo() {
-  updatePassword = Math.random();
-  $.ajax({
-    url: ajaxHandlerScript, type: "POST", cache: false, dataType: "json",
-    data: {f: "LOCKGET", n: stringName, p: updatePassword},
-    success: lockGetReady, error: errorHandler
-  })
-}
-
-function lockGetReady(callresult) {
-  if (callresult.error != undefined) {
-    alert(callresult.error);
-  } else {
-    var newInfo = {
-      newArray: info
-    }
-
-    $.ajax({
-      url: ajaxHandlerScript, type: "POST", cache: false, dataType: "json",
-      data: {f: "UPDATE", n: stringName, v: JSON.stringify(newInfo), p: updatePassword},
-      success: updateReady, error: errorHandler
-    })
-  }
-}
 
 function readInfo() {
   $.ajax({
@@ -123,19 +65,17 @@ function readInfo() {
 }
 
 function readReady(callresult) {
+  console.log(callresult);
   if (callresult.error != undefined) {
     alert(callresult.error);
   } else if (callresult.result != "") {
     var newInfo = JSON.parse(callresult.result);
-    infoFromServer = newInfo.newArray;
+    console.log(newInfo.form1);
+    form1 = newInfo.form1;
+    form2 = newInfo.form2;
   }
 }
 
-function updateReady(callresult) {
-  if (callresult.error != undefined) {
-    alert(callresult.error);
-  }
-}
 
 function errorHandler(jqXHR, statusStr, errorStr) {
   alert(statusStr + " " + errorStr);
